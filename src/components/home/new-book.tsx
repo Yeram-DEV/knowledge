9f7b879dff67371b4f015c4c8a288f9c480464c3
kor-bim <dbsgksqlschl@gmail.com>
2024-06-25 18:10:15 +0900

    Initial commit from Create Next App

--- src/components/home/new-book.tsx
+++ src/components/home/new-book.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { Swiper, SwiperSlide } from 'swiper/react'
-import { Autoplay, EffectCoverflow } from 'swiper/modules'
+import { Autoplay } from 'swiper/modules'
 import 'swiper/css'
 import 'swiper/css/pagination'
 import { Card, CardBody } from '@nextui-org/card'
