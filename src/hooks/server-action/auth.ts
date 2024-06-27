1d7abac2ed9a0aa52fe82064114ce963215ed454
kor-bim <dbsgksqlschl@gmail.com>
2024-06-27 10:09:57 +0900

    Initial commit from Create Next App

--- src/hooks/server-action/auth.ts
+++ src/hooks/server-action/auth.ts
@@ -2,11 +2,11 @@
 import { signIn } from '@/auth'
 
 export const signInWithGoogle = async () => {
-  await signIn('google')
+  await signIn('google', { redirectTo: '/' })
 }
 export const signInWithNaver = async () => {
-  await signIn('naver')
+  await signIn('naver', { redirectTo: '/' })
 }
 export const signInWithKakao = async () => {
-  await signIn('kakao')
+  await signIn('kakao', { redirectTo: '/' })
 }
