de5d6a69878ab4ce1bd14d9673278ba9b2b6b5fc
kor-bim <dbsgksqlschl@gmail.com>
2024-07-10 17:51:31 +0900

    Initial commit from Create Next App

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -13,7 +13,7 @@ export default function LoginPage() {
     await supabase.auth.signInWithOAuth({
       provider: 'google',
       options: {
-        redirectTo: process.env.NEXT_PUBLIC_REDIRECT_URL
+        redirectTo: '/auth/confirm'
       }
     })
   }
