e13015c0edf14ae645b10b05a829e4de5adda71a
kor-bim <dbsgksqlschl@gmail.com>
2024-07-11 09:17:49 +0900

    Initial commit from Create Next App

--- src/app/auth/callback/route.ts
+++ src/app/auth/callback/route.ts
@@ -1,29 +1,28 @@
 import { NextResponse } from 'next/server'
-import { createClient } from '@/utils/supabase/server'
 
 export async function GET(request: Request) {
   const { searchParams, origin } = new URL(request.url)
 
   const code = searchParams.get('code')
 
-  const next = searchParams.get('next') ?? '/'
+  // const next = searchParams.get('next') ?? '/'
 
   if (code) {
-    const supabase = createClient()
+    // const supabase = createClient()
+    //
+    // const {
+    //   error,
+    //   data: { user }
+    // } = await supabase.auth.exchangeCodeForSession(code)
 
-    const {
-      error,
-      data: { user }
-    } = await supabase.auth.exchangeCodeForSession(code)
-
-    if (!error) {
-      if (!user.user_metadata.role || !user.user_metadata.position || !user.user_metadata.team) {
-        return NextResponse.redirect(`${origin}/signup`)
-      }
-      return NextResponse.redirect(`${origin}${next}`)
-    } else {
-      console.log(error.message)
-    }
+    // if (!error) {
+    //   if (!user.user_metadata.role || !user.user_metadata.position || !user.user_metadata.team) {
+    return NextResponse.redirect(`${origin}/signup`)
+    // }
+    // return NextResponse.redirect(`${origin}${next}`)
+    // } else {
+    //   console.log(error.message)
+    // }
   }
 
   // return the user to an error page with instructions
