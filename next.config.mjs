ea3ce9aedf4ca04fcbe56c9d1c28b92a788c0d22
kor-bim <dbsgksqlschl@gmail.com>
2024-07-19 13:59:09 +0900

    Initial commit from Create Next App

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,10 @@
+import next_pwa from 'next-pwa'
+
+const withPWA = next_pwa({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development' // 개발 모드에서는 PWA 비활성화
+})
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   poweredByHeader: false,
@@ -11,4 +18,4 @@ const nextConfig = {
   }
 }
 
-export default nextConfig
+export default withPWA(nextConfig)
