0d161075af56ec88fa22a4e3996e176d32a5b4ac
kor-bim <dbsgksqlschl@gmail.com>
2024-07-19 14:56:29 +0900

    Initial commit from Create Next App

--- src/app/(dashboard)/_hooks/use-notification.ts
+++ src/app/(dashboard)/_hooks/use-notification.ts
@@ -1,17 +1,20 @@
 import { useCallback, useEffect, useState } from 'react'
 import { createClient } from '@/utils/supabase/client'
 import { Notifications } from '@/types'
+import useFcmToken from '@/hooks/use-fcm-token.tsx'
 
 export const useNotification = (user) => {
   const [notifications, setNotifications] = useState<Notifications[]>([])
   const supabase = createClient()
-
+  const { token } = useFcmToken()
   const fetchNotifications = useCallback(async () => {
+    const { data: tokenData } = await supabase.from('token').select('*').eq('fcm_token', token)
     const { data, error } = await supabase
       .from('notifications')
       .select('*')
       .eq('user_id', user.id)
       .eq('is_read', false)
+      .eq('token_id', tokenData.id)
       .order('id', { ascending: false })
 
     if (error) {
@@ -19,7 +22,7 @@ export const useNotification = (user) => {
     } else {
       setNotifications(data)
     }
-  }, [supabase, user.id])
+  }, [supabase, token, user.id])
 
   const markAsRead = async (id: number) => {
     const { error } = await supabase.from('notifications').update({ is_read: true }).eq('id', id)
