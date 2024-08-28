2b68def2bc213576f8ca3b2e3a8cd87ee05d41e1
kor-bim <dbsgksqlschl@gmail.com>
2024-08-28 10:59:17 +0900

    Initial commit from Create Next App

--- src/app/(dashboard)/(community)/event/[slug]/page.tsx
+++ src/app/(dashboard)/(community)/event/[slug]/page.tsx
@@ -1,8 +1,7 @@
 import { createClient } from '@/utils/supabase/server'
 import { Card, CardBody, CardHeader } from '@nextui-org/card'
 import { kstFormat } from '@/utils/date'
-import { NotionAPI } from 'notion-client'
-import { NotionRender } from '@/components/etc'
+import { notFound } from 'next/navigation'
 
 export default async function EventDetailPage({ params }: { params: { slug: string } }) {
   const supabase = createClient()
@@ -10,25 +9,19 @@ export default async function EventDetailPage({ params }: { params: { slug: stri
   const { data: event, error } = await supabase.from('events').select('*').eq('event_id', params.slug).maybeSingle()
 
   if (!event || error) {
-    return <div>Error loading event</div>
+    notFound()
   }
 
-  const notion = new NotionAPI()
-  const recordMap = await notion.getPage(event.content)
-
-  try {
-    return (
-      <Card fullWidth isBlurred shadow="none" className="p-4 sm:p-12">
-        <CardHeader className="w-full flex items-end justify-between">
-          <span className="text-2xl sm:text-5xl font-black">{event.event_name}</span>
-          <span className="text-tiny text-default-500">{kstFormat(new Date(event.created_at), 'yyyy-MM-dd')}</span>
-        </CardHeader>
-        <CardBody className="!max-w-full p-0">
-          <NotionRender recordMap={recordMap} />
-        </CardBody>
-      </Card>
-    )
-  } catch (err) {
-    return <div>{err.message}</div>
-  }
+  return (
+    <Card fullWidth isBlurred shadow="none" className="p-4 sm:p-12">
+      <CardHeader className="w-full flex items-end justify-between">
+        <span className="text-2xl sm:text-5xl font-black">{event.event_name}</span>
+        <span className="text-tiny text-default-500">{kstFormat(new Date(event.created_at), 'yyyy-MM-dd')}</span>
+      </CardHeader>
+      <CardBody
+        className="relative w-full !max-w-full prose dark:prose-invert px-2 sm:px-12 py-4"
+        dangerouslySetInnerHTML={{ __html: event.content }}
+      ></CardBody>
+    </Card>
+  )
 }
